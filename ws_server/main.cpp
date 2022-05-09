6b12437f0f00ea817ebe85076fd07fea28002ea0
Arcady <admin@arcirk.ru>
2022-05-09 20:26:32 +0800

    update

--- ws_server/main.cpp
+++ ws_server/main.cpp
@@ -1,23 +1,3 @@
-//
-// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-// Official repository: https://github.com/vinniefalco/CppCon2018
-//
-
-//------------------------------------------------------------------------------
-/*
-    WebSocket chat server, multi-threaded
-
-    This implements a multi-user chat room using WebSocket. The
-    `io_context` runs on any number of threads, specified at
-    the command line.
-
-*/
-//------------------------------------------------------------------------------
-
 #ifdef _WINDOWS
 #include <SDKDDKVer.h>
 #endif
